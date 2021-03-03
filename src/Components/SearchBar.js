11351d3256bf1c03c5c71efea8e227671e7b8af9
RamjithRamadas <ramjithramadas@gmail.com>
2021-03-03 23:01:00 +0530

    changed searchbar

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,21 +1,20 @@
 import { Paper, TextField } from "@material-ui/core";
 import { useState } from "react";
 
-export default function SearchBar() {
+export default ({ onSubmit }) => {
    const [searchTerm, setSearchTerm] = useState("");
-   //for taking input from user
-   const handleChange = (e) => setSearchTerm(e.target.value);
 
-   //for taking values from the state when user click serch button
-   function handleSubmit(e) {
-      e.preventDefault();
-      onFormSubmit(searchTerm);
-   }
+   const handleChange = (event) => setSearchTerm(event.target.value);
+
+   const onKeyPress = (event) => {
+      if (event.key === "Enter") {
+         onSubmit(searchTerm);
+      }
+   };
+
    return (
       <Paper elevation={6} style={{ padding: "25px" }}>
-         <form onSubmit={handleSubmit}>
-            <TextField fullWidth label="Search" onChange={handleChange}></TextField>
-         </form>
+         <TextField fullWidth label="Search..." value={searchTerm} onChange={handleChange} onKeyPress={onKeyPress} />
       </Paper>
    );
-}
+};
